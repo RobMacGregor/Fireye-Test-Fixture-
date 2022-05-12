7fdf53361dcbe5814a91ae7593983ae853eec43f
RobMacGregor <robmac8138@gmail.com>
2022-05-12 12:34:10 -0400

    Update Fireye.py

--- Fireye.py
+++ Fireye.py
@@ -2,13 +2,14 @@
 >>> import time
 >>> POC = machine.Pin(0, machine.Pin.OUT)
 >>> interlock = machine.Pin(1, machine.Pin.OUT)
-                           
+>>> fan = machine.Pin(2, machine.Pin.OUT)                  
 >>> for i in range(10):
 ...     POC.on()  
-... 
-
+...     interlock.on()
+...     fan.off()
 
 ...     time.sleep(10)
-...     pin.off()
-
+...     POC.off()
+...     fan.on()
 ...     time.sleep(60)
+
